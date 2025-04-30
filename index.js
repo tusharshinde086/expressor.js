6baabafe780c70939758c651bacc63802bb410c3
Tushar Shinde <tusharshindets086@gmail.com>
2025-04-30 21:24:57 +0530

    resonse  by res.send()

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-// console.dir(app);
 
+// Route for root URL
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
-//--------------------------------
+
+// Middleware (should be before routes if it's global)
+
+// app.use --------------------------
+app.use((req, res, next) => {
+  console.log("Request received");
+  next(); // Important to allow the request to continue
+
+res.send("This is a basic respnse ");
+
+});
+
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
-
-//app.use  --------------
-
-app.use((req,res)=> {
-  console.log("request rceive");
+  console.log(`Example app listening on port ${port}`);
 });
