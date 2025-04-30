36b78015891797d8cb5b1e742e9a32cf9d9fd13e
Tushar Shinde <tusharshindets086@gmail.com>
2025-04-30 21:29:09 +0530

    resonse  by res.send()

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8000;
 
-// Route for root URL
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 // Middleware (should be before routes if it's global)
 
 // app.use --------------------------
 app.use((req, res, next) => {
   console.log("Request received");
-  next(); // Important to allow the request to continue
-
+  
+res.send("This is a basic respnse ");
 
 });
 
