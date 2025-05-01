7eee4deb360645997e9ef08f2d44cee4f9c922c3
Tushar Shinde <tusharshindets086@gmail.com>
2025-05-01 23:30:07 +0530

    req.params multiple net

--- req.params.js
+++ req.params.js
@@ -2,15 +2,31 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-app.get("/", (req, res) => {
-    res.send("Hello, I am ts");
+app.get("/",(req,res)=>{
+    res.send("hello i am ts ");
 });
 
-app.get("/:username/:id", (req, res) => {
-    let { username, id } = req.params;
-    res.send(`Welcome to the page of @${username} with ID ${id}`);
+// app.get("/:names",(req,res)=>{
+//      console.log(req.params); 
+//     res.send("hello i m root");
+// });
+
+//---------------------------------------------------------
+
+// app.get("/:username/:id",(req,res)=>{
+    
+//     let {username , id }= req.params;
+//      htmlStr=`<h1>welcome to the page of @${username}!</h1>;
+// });
+//--------------------------------------------------------------
+app.get("/:username/:id",(req,res)=>{
+    
+    let {username , id }= req.params;
+   res.send(`welcome to the page of @${username},`);
 });
 
+// Start the server
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
-});
+  });
+  
